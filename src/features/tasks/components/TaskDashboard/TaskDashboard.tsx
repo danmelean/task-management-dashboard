b4bee3caa499964c9bb24dc0dc79a5ce2db61f7c
dmelean <melean.dane@gmail.com>
2026-09-11 19:19:06 -0300

    fixes

--- src/features/tasks/components/TaskDashboard/TaskDashboard.tsx
+++ src/features/tasks/components/TaskDashboard/TaskDashboard.tsx
@@ -6,34 +6,38 @@ import { TaskTable } from "../TaskTable";
 import { createTask } from "../../services/taskService";
 import type { Task } from "../../types/Task";
 import { TaskImport } from "../TaskImport/TaskImport";
+import { Card, CardContent } from "@/components/ui/card";
 
 export function TaskDashboard() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   async function handleAddTask(title: string): Promise<void> {
-    const task = await createTask({title});
+    const task = await createTask({ title });
 
     setTasks((currentTasks) => [...currentTasks, task]);
   }
 
   return (
-    <>
-      <TaskInput onAddTask={handleAddTask} />
+    <Card className="w-full">
+      <CardContent>
+        <div className="flex flex-col md:flex-row gap-8 items-center justify-start">
+          <div className="w-full md:w-1/3">
+            <TaskInput onAddTask={handleAddTask} />
+          </div>
+          <div className="w-full md:w-2/3">
+            <TaskImport onAddTask={handleAddTask} />
+          </div>
+        </div>
+        <div>
+          <h3 className="text-xl font-semibold mt-6 mb-2">Tasks:</h3>
 
-      <div>
-        <TaskImport onAddTask={handleAddTask} />
-      </div>
-
-      <div>
-        <h3 className="text-xl font-semibold mt-6 mb-2">Tasks:</h3>
-
-        {tasks.length === 0 ? (
-          <p className="text-gray-500">No tasks yet.</p>
-        ) : (
-          <TaskTable tasks={tasks} />
-        )}
-      </div>
-      
-    </>
+          {tasks.length === 0 ? (
+            <p className="text-gray-500">No tasks yet.</p>
+          ) : (
+            <TaskTable tasks={tasks} />
+          )}
+        </div>
+      </CardContent>
+    </Card>
   );
 }
